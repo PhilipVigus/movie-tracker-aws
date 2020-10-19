f282b4f55d483b31ba42eb70ea679258e9be996d
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-19 15:52:17 +0100

    Delete console.log

--- lambdaFunctions/addTrailersToDb/parseRssFeed.js
+++ lambdaFunctions/addTrailersToDb/parseRssFeed.js
@@ -13,23 +13,30 @@ const parseTags = (tagString) => {
 
 const parseRssFeed = (data) => {
   const trailers = [];
+  
   parseString(data, (err, result) => {
-    const trailersAsJson = result.rss.channel[0].item;
+    if(err) {
+      console.log(`Error = ${err}`); 
+    } else {
+      const trailersAsJson = result.rss.channel[0].item;
 
-    trailersAsJson.forEach((trailer) => {
-      const imageLink = trailer.description[0].match(/<img src="(.*)" \/>/)[1];
-      const tags = trailer.description[0].match(/Tags: (<a.*\/a>)/)[1];
-      parseTags(tags);
-      trailers.push({
-        id: trailer.guid[0],
-        title: trailer.title[0],
-        date: trailer.pubDate[0],
-        link: trailer.link[0],
-        image: imageLink,
-        tags: parseTags(tags)
+      trailersAsJson.forEach((trailer) => {
+        const imageLink = trailer.description[0].match(/<img src="(.*)" \/>/)[1];
+        const tags = trailer.description[0].match(/Tags: (<a.*\/a>)/)[1];
+        parseTags(tags);
+        trailers.push({
+          id: trailer.guid[0],
+          title: trailer.title[0],
+          date: trailer.pubDate[0],
+          link: trailer.link[0],
+          image: imageLink,
+          tags: parseTags(tags)
+        });
       });
-    });
+    }
+
   });
+
   return trailers;
 };
 
