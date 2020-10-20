61407105959b8b28e17100c6f14fbd3c9cc96dab
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-20 14:02:59 +0100

    Delete unused import

--- lambdaFunctions/addTrailersToDb/tests/deleteTable.test.js
+++ lambdaFunctions/addTrailersToDb/tests/deleteTable.test.js
@@ -1,7 +1,6 @@
 const AWS = require('aws-sdk');
 const createTable = require("../createTable.js");
 const deleteTable = require("../deleteTable.js");
-const tableExists = require('../tableExists');
 const dbConfig = require("./fixtures/dbConfig");
 const testTableParams = require('./fixtures/testTableParams.js');
 
@@ -11,7 +10,6 @@ describe("deleteTable", () => {
 
     testTableParams.TableName = "testTable_delete"
     await createTable(db, testTableParams);
-    expect(await tableExists(db, testTableParams.TableName)).toEqual(true);
     const result = await deleteTable(db, testTableParams.TableName);
     
     expect(result).toBeDefined();
