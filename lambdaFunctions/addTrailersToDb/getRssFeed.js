f0ccaa8e710694fb0057e46f4e72301b69d2787b
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-20 11:21:31 +0100

    Simplify getRssFeed function

--- lambdaFunctions/addTrailersToDb/getRssFeed.js
+++ lambdaFunctions/addTrailersToDb/getRssFeed.js
@@ -2,12 +2,8 @@ const Axios = require("axios");
 Axios.defaults.adapter = require("axios/lib/adapters/http");
 
 const getRssData = async () => {
-  try {
-    const data = await Axios.get("https://www.traileraddict.com/rss");
-    return data;
-  } catch (e) {
-    return { error: e.message };
-  }
+  const feed = await Axios.get("https://www.traileraddict.com/rss");
+  return feed.data;
 };
 
 module.exports = getRssData;
