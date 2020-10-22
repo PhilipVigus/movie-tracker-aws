bdd2883b9b4353b2ee8f5cfda54e03a97c98d572
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-22 11:14:33 +0100

    Prevent duplicate trailers being added to the db

--- lambdaFunctions/addTrailersToDb/populateDbWithTrailers.js
+++ lambdaFunctions/addTrailersToDb/populateDbWithTrailers.js
@@ -1,18 +1,23 @@
+const trailerExists = require("./trailerExists");
+
 const populateDbWithTrailers = async (document, trailers, tableName) => {
   for (let trailer of trailers) {
-    await document.put(
-      { 
-        TableName: tableName,
-        Item: { 
-          id: trailer.id,
-          title: trailer.title,
-          date: trailer.date,
-          link: trailer.link,
-          image: trailer.image,
-          tags: trailer.tags
+    const isTrailerAlreadyInDb = await trailerExists(document, tableName, trailer.id);
+    if (!isTrailerAlreadyInDb) {
+      await document.put(
+        { 
+          TableName: tableName,
+          Item: { 
+            id: trailer.id,
+            title: trailer.title,
+            date: trailer.date,
+            link: trailer.link,
+            image: trailer.image,
+            tags: trailer.tags
+          }
         }
-      }
-    ).promise();  
+      ).promise();  
+    }
   }
 }
 
--- lambdaFunctions/addTrailersToDb/tests/populateDbWithTrailers.test.js
+++ lambdaFunctions/addTrailersToDb/tests/populateDbWithTrailers.test.js
@@ -5,6 +5,7 @@ const populateDbWithTrailers = require("../populateDbWithTrailers");
 const dbConfig = require("./fixtures/dbConfig");
 const trailerTableParams = require("../trailerTableParams");
 const parsedRssTestData = require("./fixtures/parsedRssTestData");
+const parsedRSSTestDataWithDuplicate = require("./fixtures/parsedRssTestDataWithDuplicate");
 
 describe("populateDbWithTrailers", () => {
   it("adds the trailers to the db", async () => {
@@ -17,6 +18,21 @@ describe("populateDbWithTrailers", () => {
     const tableData = await db.describeTable({ TableName: trailerTableParams.TableName }).promise();
     
     expect(tableData.Table.ItemCount).toEqual(2);
+
+    await deleteTable(db, trailerTableParams.TableName);
+  });
+
+  it("prevents duplicate trailers being added to the db", async () => {
+    const db = new AWS.DynamoDB(dbConfig);
+    const document = new AWS.DynamoDB.DocumentClient(dbConfig);
+
+    trailerTableParams.TableName = "testTable_preventDuplicates";
+    await createTable(db, trailerTableParams);
+    await populateDbWithTrailers(document, parsedRssTestData, trailerTableParams.TableName);
+    await populateDbWithTrailers(document, parsedRSSTestDataWithDuplicate, trailerTableParams.TableName);
+    const tableData = await db.describeTable({ TableName: trailerTableParams.TableName }).promise();
+    
+    expect(tableData.Table.ItemCount).toEqual(3);
 
     await deleteTable(db, trailerTableParams.TableName);
   });
